8e4c4396225142a4eae0c7e4362cd40ce4158a2a
danil <danil@DESKTOP-KLFI3LI>
2022-11-13 23:15:29 +0200

    Добавьте файлы проекта.

---
+++ 6.108/Program.cs
@@ -0,0 +1,16 @@
+﻿//Голубихин Данила 22-ИСП-2/2 Вариант 7
+Console.Write("Введите х0: ");
+double x0 = double.Parse(Console.ReadLine());
+Console.Write("Введите хk: ");
+double xk = double.Parse(Console.ReadLine());
+Console.Write("Введите dх: ");
+double dx = double.Parse(Console.ReadLine());
+Console.Write("Введите b: ");
+double b = double.Parse(Console.ReadLine());
+double x = x0;
+while (x <= (xk + dx / 2))
+{
+    double y = 9*(x+15*Math.Sqrt(Math.Pow(x,3)+Math.Pow(b,3)));
+    Console.WriteLine($"| x={x:F2} | y={y:F2} |");
+    x+=dx;
+}
